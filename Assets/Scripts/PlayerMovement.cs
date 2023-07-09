d376842c5c11db6c9513e8a8f0db051b35486ba1
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2023-07-09 03:11:48 -0700

    made crouch feel good in game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,55 +3,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	public CharacterController2D controller;
-	public Animator animator;
+    public CharacterController2D controller;
+    public Animator animator;
 
-	public float runSpeed = 20f;
+    private float runSpeed = 20f;
 
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool crouch = false;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
-	// Update is called once per frame
-	void Update () {
+    private float crouchToRunTimer = 0f;
+    private bool crouchToRunTimerStart = false;
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    // Update is called once per frame
+    void Update()
+    {
 
-		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetButtonDown("Jump"))
-		{
-			jump = true;
-			animator.SetBool("IsJumping", true);
-		}
+        // If shift is held down, player will speed will increase to 40f 
+        if (Input.GetButton("Run"))
+        {
+            runSpeed = 40f;
+            if (crouchToRunTimerStart == true)
+            {
+                if (crouchToRunTimer >= 0.5f)
+                {
+                    crouchToRunTimer = 0f;
+                    crouchToRunTimerStart = false;
+                }
+                else
+                {
+                    runSpeed = 20f;
+                    crouchToRunTimer += Time.deltaTime;
 
-		if (Input.GetButtonDown("Crouch"))
-		{
-			crouch = true;
-		} else if (Input.GetButtonUp("Crouch"))
-		{
-			crouch = false;
-		}
+                }
+            }
+            if (crouch == true && horizontalMove == 0)
+            {
+                runSpeed = 20f;
+                crouchToRunTimerStart = true;
+            }
+            else
+            {
 
-	}
+                crouch = false;
+            }
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
 
-	public void OnLanding ()
-	{
-		animator.SetBool("IsJumping", false);
-	}
+            runSpeed = 20f;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
 
-	public void OnCrouching (bool isCrouching)
-	{
-		animator.SetBool("IsCrouching", isCrouching);
-	}
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = !crouch;
+        }
 
-	void FixedUpdate ()
-	{
-		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-		jump = false;
-	}
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
 }
