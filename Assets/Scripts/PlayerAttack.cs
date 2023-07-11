269a61c5c09c934a95dd62c1cdd5c2b0b13ae158
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2023-07-10 19:18:03 -0700

    final commit for now
    Need to fix gun targetting bug

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -45,32 +45,58 @@ public class PlayerAttack : MonoBehaviour
 
             // Calculate the rotation needed to point the object at the target
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            angle = Mathf.Clamp(angle, -50, 90);
+
+            // Check if the player is facing left, and adjust angle calculation if true
+            if (transform.eulerAngles.y == 180)
+            {
+                armTransform.localScale = new Vector3(-1, 1, 1);
+                lineRenderer.transform.localScale = new Vector3(-1, 1, 1);
+                angle = Mathf.Clamp(angle + 180, 230, 90);
+            }
+            else
+            {
+                armTransform.localScale = new Vector3(1, 1, 1);
+                lineRenderer.transform.localScale = new Vector3(1, 1, 1);
+                angle = Mathf.Clamp(angle, -50, 90);
+            }
+
             armTransform.rotation = Quaternion.Euler(0, 0, angle);
 
-            RaycastHit2D hit = Physics2D.Raycast(lineRenderer.transform.position, direction);
-            lineRenderer.positionCount = 2;
+            // Direction for the raycast
+            Vector3 raycastDirection = lineRenderer.transform.right;
 
+            // Check if the player is facing left and adjust raycast direction if true
+            if (transform.eulerAngles.y == 180)
+            {
+                raycastDirection = -raycastDirection;
+                armTransform.gameObject.GetComponentInChildren<SpriteRenderer>().flipY = true;
+            }
+
+            RaycastHit2D hit = Physics2D.Raycast(lineRenderer.transform.position, raycastDirection, 1000f);
+            lineRenderer.positionCount = 2;
 
             if (hit)
             {
-                // If raycast hits something, set end point to hit point
                 lineRenderer.SetPosition(0, lineRenderer.transform.position);
                 lineRenderer.SetPosition(1, hit.point);
             }
             else
             {
-                // If raycast hits nothing, draw line towards mouse position
                 lineRenderer.SetPosition(0, lineRenderer.transform.position);
                 lineRenderer.SetPosition(1, lineRenderer.transform.position + direction.normalized * 0.5f); // Line length when nothing is hit
             }
             // OnGunFire();
         }
-
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            OnGunFire();
+        }
     }
+
 
     private void OnGunFire()
     {
+        lineRenderer.positionCount = 0;
         playerMovementScript.enabled = true;
         selfSpriteRenderer.enabled = true;
         foreach (SpriteRenderer renderer in childRenderers)
